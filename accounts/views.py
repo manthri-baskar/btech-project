e4168e60a2f2402b9b3edc4f52ecdb59a59366c3
manthri-baskar <mrbroyal.9701@gmail.com>
2020-11-29 09:55:34 +0530

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from accounts.forms import SignUpForm
-
 # Sign Up View
 class SignUpView(CreateView):
     form_class = SignUpForm
